316c2b75fac6a3e344ec92b5d3785b860411c33c
root <root@LAPTOP-S97H7TV9.localdomain>
2021-11-17 17:14:18 +0700

    JAWABAN UTS SOAL 3

--- minggu6/source.cpp
+++ minggu6/source.cpp
@@ -3,8 +3,11 @@ void cetakUmur(int *umur){
  printf("Halo, Umur kamu adalah %d tahun", *umur);
 }
 int main(){
- int angka=20;
- cetakUmur(&angka);
- getchar();
- return 0;
+
+   int angka=20;
+   cetakUmur(&angka);
+   getchar();
+  
+   return 0;
+
 }
