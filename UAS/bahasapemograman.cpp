da668e9d6d7762db671f1e26eb31cd96be272fcc
root <root@LAPTOP-S97H7TV9.localdomain>
2022-01-26 21:35:50 +0700

    JAWABAN UAS

---
+++ UAS/bahasapemograman.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    string nama = "Bagas Djunaedi";
+    string nim = "20200801166";
+
+    cout<<"Nama saya adalah "<<nama<<endl;
+    cout<< "Alamat nama memory : ";
+    cout<< &nama <<endl;
+
+    cout <<"NIM saya adalah "<<nim<<endl;
+    cout << "Alamat NIM memory : ";
+    cout << &nim <<endl;
+}
---
+++ UAS/nomor2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void nama(){
+    cout<<"Nama Saya adalah Bagas Djunaedi"<<endl;
+}
+
+void nim(){
+    cout<<"NIM Saya adalah 20200801166"<<endl;
+}
+
+int main(){
+    nama();
+    nim();
+}
